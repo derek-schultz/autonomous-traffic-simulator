a3235f948e3ed243faa5ab44488051e43924035c
Derek Schultz <derek@derekschultz.net>
2013-04-27 20:32:45 -0400

    Adds some mapping functions

--- ats.c
+++ ats.c
@@ -21,6 +21,10 @@
 #define MAP_WIDTH 64
 #define MAP_HEIGHT 64
 
+// Ask Jeremy what these are
+#define NUM_VP_X 32
+#define NUM_VP_Y 32
+
 // Maximum number of cars allowed on a lane:
 #define MAX_CARS 30
 
@@ -119,10 +123,113 @@ void intersection_reverse_eventhandler(intersection_state*, tw_bf*, message_data
 // Function to collection statistics for an intersection:
 void intersection_statistics_collectstats(intersection_state*, tw_lp *);
 
+// Mapping functions
+tw_peid cell_mapping_lp_to_pe(tw_lpid lpid);
+tw_lp* cell_mapping_to_lp(tw_lpid lpid);
+tw_lpid cell_mapping_to_local_index(tw_lpid lpid);
+void traffic_grid_mapping();
+tw_lpid cell_compute_move(tw_lpid lpid, int direction);
+
 /*******************************END FUNCTION PROTOTYPES***********************/
+
+// Function roles:
+tw_lptype mylps[] = {
+    {
+        (init_f) intersection_startup,
+        (event_f) intersection_eventhandler,
+        (revent_f) intersection_reverse_eventhandler,
+        (final_f) intersection_statistics_collectstats,
+        (map_f) cell_mapping_lp_to_pe,
+        sizeof(intersection_state)
+    },
+    { 0 },
+};
 
 // Main Function:
 int main(int argc, char* argv[]) {
 
 	return 0;
 } /** END FUNCTION main **/
+
+tw_peid cell_mapping_lp_to_pe(tw_lpid lpid) {
+    long lp_x = lpid % MAP_WIDTH;
+    long lp_y = lpid / MAP_WIDTH;
+    long vp_num_x = lp_x / g_cells_per_vp_x;
+    long vp_num_y = lp_y / g_cells_per_vp_y;
+    long vp_num = vp_num_x + (vp_num_y * NUM_VP_X);  
+    tw_peid peid = vp_num/g_vp_per_proc;  
+    return peid;
+}
+
+tw_lp* cell_mapping_to_lp(tw_lpid lpid) {
+    tw_lpid lp_x = lpid % MAP_WIDTH; //lpid -> (lp_x,lp_y)
+    tw_lpid lp_y = lpid / MAP_WIDTH;
+    tw_lpid vp_index_x = lp_x % g_cells_per_vp_x;
+    tw_lpid vp_index_y = lp_y % g_cells_per_vp_y;
+    tw_lpid vp_index = vp_index_x + (vp_index_y * (g_cells_per_vp_x));
+    tw_lpid vp_num_x = lp_x / g_cells_per_vp_x;
+    tw_lpid vp_num_y = lp_y / g_cells_per_vp_y;
+    tw_lpid vp_num = vp_num_x + (vp_num_y * NUM_VP_X);
+    vp_num = vp_num % g_vp_per_proc;
+    tw_lpid index = vp_index + vp_num*g_cells_per_vp;
+
+#ifdef ROSS_runtime_check  
+    if (index >= g_tw_nlp)
+        tw_error(TW_LOC, "index (%llu) beyond g_tw_nlp (%llu) range \n", index, g_tw_nlp);
+#endif /* ROSS_runtime_check */
+
+    return g_tw_lp[index];
+}
+
+tw_lpid cell_mapping_to_local_index(tw_lpid lpid) {
+    tw_lpid lp_x = lpid % MAP_WIDTH; //lpid -> (lp_x,lp_y)
+    tw_lpid lp_y = lpid / MAP_WIDTH;
+    tw_lpid vp_index_x = lp_x % g_cells_per_vp_x;
+    tw_lpid vp_index_y = lp_y % g_cells_per_vp_y;
+    tw_lpid vp_index = vp_index_x + (vp_index_y * (g_cells_per_vp_x));
+    tw_lpid vp_num_x = lp_x/g_cells_per_vp_x;
+    tw_lpid vp_num_y = lp_y/g_cells_per_vp_y;
+    tw_lpid vp_num = vp_num_x + (vp_num_y * NUM_VP_X);  
+    vp_num = vp_num % g_vp_per_proc;
+    tw_lpid index = vp_index + vp_num * g_cells_per_vp;
+
+    if (index >= g_tw_nlp)
+        tw_error(TW_LOC, "index (%llu) beyond g_tw_nlp (%llu) range \n", index, g_tw_nlp);
+
+    return index;
+}
+
+void traffic_grid_mapping() {
+    tw_lpid x, y;
+    tw_lpid lpid, kpid;
+    tw_lpid num_cells_per_kp, vp_per_proc;
+    tw_lpid local_lp_count;
+
+    num_cells_per_kp = (MAP_WIDTH * MAP_HEIGHT) / (NUM_VP_X * NUM_VP_Y);
+    vp_per_proc = (NUM_VP_X * NUM_VP_Y) / ((tw_nnodes() * g_tw_npe)) ;
+    g_tw_nlp = nlp_per_pe;
+    g_tw_nkp = vp_per_proc;
+
+    local_lp_count = 0;
+    for (y = 0; y < MAP_HEIGHT; y++)
+    {
+        for (x = 0; x < MAP_WIDTH; x++)
+        {
+            lpid = (x + (y * MAP_WIDTH));
+            if (g_tw_mynode == cell_mapping_lp_to_pe(lpid)) {
+                kpid = local_lp_count / num_cells_per_kp;
+                local_lp_count++; // MUST COME AFTER!! DO NOT PRE-INCREMENT ELSE KPID is WRONG!!
+
+                if (kpid >= g_tw_nkp)
+                    tw_error(TW_LOC, "Attempting to mapping a KPid (%llu) for Global LPid %llu that is beyond g_tw_nkp (%llu)\n",
+                    kpid, lpid, g_tw_nkp );
+
+                tw_lp_onpe(cell_mapping_to_local_index(lpid), g_tw_pe[0], lpid);
+                if (g_tw_kp[kpid] == NULL)
+                    tw_kp_onpe(kpid, g_tw_pe[0]);
+                tw_lp_onkp(g_tw_lp[cell_mapping_to_local_index(lpid)], g_tw_kp[kpid]);
+                tw_lp_settype(cell_mapping_to_local_index(lpid), &mylps[0]);
+            }
+        }
+    }
+}
