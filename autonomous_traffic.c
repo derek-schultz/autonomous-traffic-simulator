692f7a4087b52f45e020f3cbcfbadb9bb3ce7edd
Derek Schultz <derek@derekschultz.net>
2013-05-05 23:41:32 -0400

    Removed an unneeded function (it was moved and reformatted)

--- autonomous_traffic.c
+++ autonomous_traffic.c
@@ -1,40 +1,5 @@
 #include "ats.h"
 
-/*
- * Determines whether cars a and b will colide if they travel through the
- * intersection at the same time.
- * NL indicates that the car is coming from the north and turning left
- * ES indicates that the car is coming from the east and going stratight
- * etc...
- */
-int will_collide(enum travel_direction a, enum travel_direction b) {
-    if (a == NL && (b == SS || b == SR || b == ES || b == EL || b == WS || b == WL))
-        return 1;
-    if (a == NR && (b == SL || b == ES))
-        return 1;
-    if (a == NS && (b == SL || b == ES || b == EL || b == WS || b == WR || b == WL))
-        return 1;
-    if (a == EL && (b == SS || b == SL || b == NS || b == NL || b == WR || b == WS))
-        return 1;
-    if (a == ER && (b == SS || b == WL))
-        return 1;
-    if (a == ES && (b == SS || b == SL || b == NL || b == NR || b == NS || b == WL))
-        return 1;
-    if (a == SL && (b == ES || b == EL || b == NR || b == NS || b == WL || b == WS))
-        return 1;
-    if (a == SR && (b == WS || b == NL))
-        return 1;
-    if (a == SS && (b == NL || b == ES || b == EL || b == ER || b == WS || b == WL))
-        return 1;
-    if (a == WL && (b == NS || b == NL || b == SS || b == SL || b == ES || b == ER))
-        return 1;
-    if (a == WR && (b == EL || b == NS))
-        return 1;
-    if (a == WS && (b == SS || b == SL || b == SR || b == EL || b == NS || b == NL))
-        return 1;
-    return 0;
-}
-
 void autonomous_traffic_intersection_startup(intersection_state* SV,
                                              tw_lp* LP) {
     tw_stime ts = 0;
